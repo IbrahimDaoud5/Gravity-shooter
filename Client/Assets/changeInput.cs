0cbd8355be9612b8586f1ecbd15b658cf5e92730
IbrahimDaoud5 <99076315+IbrahimDaoud5@users.noreply.github.com>
2023-12-09 15:40:27 +0200

    Update changeInput.cs
    try catch added to prevent exeptions when pressing tabs

--- Client/Assets/changeInput.cs
+++ Client/Assets/changeInput.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class changeInput : MonoBehaviour
 {
@@ -19,21 +18,30 @@ public class changeInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift)) {
-            Selectable previous = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
-            if(previous != null)
-            {
-                previous.Select();
-            }
-        }
-       else if (Input.GetKeyDown(KeyCode.Tab))
+        try
         {
-            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
-            if (next != null)
+            if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
             {
-                next.Select();
+                Selectable previous = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+                if (previous != null)
+                {
+                    previous.Select();
+                }
             }
+            else if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+                if (next != null)
+                {
+                    next.Select();
+                }
 
+            }
         }
+        catch (Exception e)
+        {
+            Debug.LogError(e.ToString()); // This will print the entire exception, including the stack trace
+        }
+
     }
 }
