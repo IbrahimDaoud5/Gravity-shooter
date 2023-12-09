c97a4b1b62731559f1c159573c8a0411651452cb
yosifhousen <yosif.hosen@e.braude.ac.il>
2023-12-09 15:54:01 +0200

    tiny changes
    tiny changes

--- Client/Assets/Scripts/ChangeInput.cs
+++ Client/Assets/Scripts/ChangeInput.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class changeInput : MonoBehaviour
+public class ChangeInput : MonoBehaviour
 {
     EventSystem system;
     public Selectable firstInput;
