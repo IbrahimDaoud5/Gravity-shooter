d7f4f53e3428ccc90081bfb5f375810e78c192f9
yosifhousen <yosif.hosen@e.braude.ac.il>
2024-01-09 15:31:32 +0200

    test for yosif plugins

--- Server/src/main/java/com/example/server/ServerApplication.java
+++ Server/src/main/java/com/example/server/ServerApplication.java
@@ -2,7 +2,7 @@ package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hi
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
