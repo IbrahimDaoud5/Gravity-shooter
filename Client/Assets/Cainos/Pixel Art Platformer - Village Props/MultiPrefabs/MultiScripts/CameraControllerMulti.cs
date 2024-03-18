82033248f682394e87ef56112337438518a86b74
IbrahimDaoud5 <99076315+IbrahimDaoud5@users.noreply.github.com>
2024-03-18 11:58:26 +0200

    RL

--- Client/Assets/Cainos/Pixel Art Platformer - Village Props/MultiPrefabs/MultiScripts/CameraControllerMulti.cs
+++ Client/Assets/Cainos/Pixel Art Platformer - Village Props/MultiPrefabs/MultiScripts/CameraControllerMulti.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -8,6 +6,6 @@ public class CameraControllerMultis : NetworkBehaviour
     [SerializeField] private Transform player;
     void Update()
     {
-        transform.position = new Vector3(player.position.x,player.position.y,transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
